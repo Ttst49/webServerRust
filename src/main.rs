6107b6530e96007e37bfd6b12a11ca81b20ef21f
thibaut.stachnick <thibautstachnick@gmail.com>
2024-05-07 20:47:09 +0200

    bind ipAddress and listen to flows and return a response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::net::TcpListener;
+
+
 fn main() {
-    println!("Hello, world!");
+    let listener =
+        TcpListener::bind("127.0.0.1:9999").unwrap();
+
+    for flux in listener.incoming() {
+        let flux = flux.unwrap();
+
+        println!("Connexion established")
+    }
 }
