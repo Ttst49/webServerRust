9a114c07243ccba27e10340083b33eec2f9f2a2e
thibaut.stachnick <thibautstachnick@gmail.com>
2024-05-07 21:15:35 +0200

    add a way to use request with flow and buffer as a new function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use std::net::TcpListener;
-
+use std::io::prelude::*;
+use std::net::TcpStream;
 
 fn main() {
     let listener =
         TcpListener::bind("127.0.0.1:9999").unwrap();
 
-    for flux in listener.incoming() {
-        let flux = flux.unwrap();
+    for flow in listener.incoming() {
+        let flow = flow.unwrap();
 
-        println!("Connexion established")
+        manage_connection(flow)
     }
+}
+
+fn manage_connection(mut flow:TcpStream){
+    let mut buffer = [0;1024];
+
+    flow.read(&mut buffer).unwrap();
+
+    println!("Request :{}",String::from_utf8_lossy(&buffer[..]));
 }
