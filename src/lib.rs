39b1da9b36e2d344f160159911f012bd7b1b9c9a
thibaut.stachnick <thibautstachnick@gmail.com>
2024-05-08 17:02:35 +0200

    add Operators struct and use it in ThreadPool

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,34 @@
-pub struct ThreadPool;
+use std::thread;
+
+
+pub struct ThreadPool{
+    operators:Vec<Operator>,
+}
 
 impl ThreadPool {
     pub fn new(size:usize)->ThreadPool{
         assert!(size>0);
-        ThreadPool
+        let mut operators = Vec::with_capacity(size);
+        for id in 0..size {
+            operators.push(Operator::new(id))
+        }
+        ThreadPool{operators}
     }
 
     pub fn execute<F>(&self, f:F)
     where F: FnOnce()+ Send + 'static,{
 
+    }
+}
+
+struct Operator{
+    id:usize,
+    task:thread::JoinHandle<()>
+}
+
+impl Operator {
+    fn new(id:usize)->Operator{
+        let task = thread::spawn(||{});
+        Operator{id, task}
     }
 }
