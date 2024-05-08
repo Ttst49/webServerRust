2be0f3f754b512df7cd6ee7156f8aa24de1131a2
thibaut.stachnick <thibautstachnick@gmail.com>
2024-05-08 15:12:50 +0200

    add a condition if you try to access some bad route

--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,32 @@ fn manage_connection(mut flow:TcpStream){
     let mut buffer = [0;1024];
     flow.read(&mut buffer).unwrap();
 
-    let content = fs::read_to_string("./src/index.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+    
+    if buffer.starts_with(get) {
+        let content = fs::read_to_string("../template/index.html").unwrap();
 
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        content.len(),
-        content
-    );
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            content.len(),
+            content
+        );
 
-    flow.write(response.as_bytes()).unwrap();
-    flow.flush().unwrap();
+        flow.write(response.as_bytes()).unwrap();
+        flow.flush().unwrap();
+    }else {
+        let line_status = "HTTP/1.1 404 NOT FOUND";
+        let content = fs::read_to_string("./template/404.html").unwrap();
+
+        let response = format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            line_status,
+            content.len(),
+            content
+        );
+
+        flow.write(response.as_bytes()).unwrap();
+        flow.flush().unwrap();
+    }
 
 }
