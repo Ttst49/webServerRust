2510172b0e00762b7ef773022820c02747d4509e
thibaut.stachnick <thibautstachnick@gmail.com>
2024-05-08 22:58:52 +0200

    implement Drop and correct code to add Message struct

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::sync::Mutex;
 
 pub struct ThreadPool{
     operators:Vec<Operator>,
-    send:mpsc::Sender<Mission>
+    send:mpsc::Sender<Message>
 }
 
 type Mission = Box<dyn FnOnce()+Send+'static>;
@@ -26,15 +26,24 @@ impl ThreadPool {
     pub fn execute<F>(&self, f:F)
     where F: FnOnce()+ Send + 'static,{
         let mission = Box::new(f);
-        self.send.send(mission).unwrap()
+        self.send.send(Message::NewMission(mission)).unwrap()
     }
 }
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        println!("Send extinction message to all operators.");
+
+        for _ in &self.operators {
+            self.send.send(Message::Extinction).unwrap();
+        }
+
+        println!("Stop all operators.");
+
         for operator in &mut self.operators {
-            println!("Stopping operation {}",operator.id);
-            if let Some(task) = operator.task.take(){
+            println!("Stopping operator {}", operator.id);
+
+            if let Some(task) = operator.task.take() {
                 task.join().unwrap();
             }
         }
@@ -47,14 +56,27 @@ struct Operator{
 }
 
 impl Operator {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Mission>>>) -> Operator {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Operator {
         let task = thread::spawn(move ||loop {
-            let mission = receiver.lock().unwrap().recv().unwrap();
-            println!("Operator {} got a mission ; it execute it.", id);
-
-            mission();
+            let message = receiver.lock().unwrap().recv().unwrap();
+            match message {
+                Message::NewMission(mission)=>{
+                    println!("Operator {} got a mission; it execute it",id);
+                    mission()
+                }
+                Message::Extinction=>{
+                    println!("Operator {} got a mission; it execute it",id);
+                    break;
+                }
+            }
         });
 
         Operator { id, task:Some(task) }
     }
+}
+
+
+enum Message{
+    NewMission(Mission),
+    Extinction
 }
